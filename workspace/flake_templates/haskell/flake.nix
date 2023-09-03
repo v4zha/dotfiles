6a1d7d206b7ab840c65a2fc2552cc4d02b16649c
arvin <arvin_j4848@protonmail.com>
2023-09-03 13:22:02 +0530

    chore : version update

--- workspace/flake_templates/haskell/flake.nix
+++ workspace/flake_templates/haskell/flake.nix
@@ -4,6 +4,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    my-package = "";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -23,7 +24,7 @@
           };
         };
 
-        packages.default = self'.packages.<your-package-name>;
+        packages.default = self'.packages.${my-package};
       };
     };
 }
