c2fcacf562bda088fb1501a4dd3703e1516f8864
V4zha <arvin_j4848@protonmail.com>
2023-09-27 07:02:06 +0000

    feat:flake8 patch
    flake 8 extended length 
    and ignore [E203](https://github.com/PyCQA/pycodestyle/issues/373)

--- .config/nvim/lua/v4zha/plugins/null.lua
+++ .config/nvim/lua/v4zha/plugins/null.lua
@@ -4,7 +4,7 @@ if not status then
 end
 
 local src = { null.builtins.formatting.black,
-  null.builtins.diagnostics.flake8,
+  null.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "88" ,"--extend-ignore","E203"} }),
 }
 
 null.setup {
