d28d985bf67eb2eafdcb304042152dd76bfb935a
V4zha <arvin_j4848@protonmail.com>
2024-11-05 15:59:14 +0530

    +add:cmake_lsp

--- .config/nvim/lua/v4zha/plugins/lsp/lspsrv.lua
+++ .config/nvim/lua/v4zha/plugins/lsp/lspsrv.lua
@@ -1,23 +1,26 @@
 local lsp_servers = { 'clangd',
 
   {
-  name='lua_ls',settings={
-  Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {'vim'},
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
+    name = 'lua_ls',
+    settings = {
+      Lua = {
+        runtime = {
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          globals = { 'vim' },
+        },
+        workspace = {
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        telemetry = {
+          enable = false,
+        },
       },
     },
-},}, 'taplo', 'html', 'ts_ls', 'cssls','hls', 'ocamllsp','clangd',
-  'pyright', 'dockerls', 'yamlls', 'rust_analyzer', 'zls', 'marksman','rnix'};
+  },
+  'cmake', 'taplo', 'hls', 'ocamllsp', 'clangd',
+  'dockerls', 'yamlls', 'rust_analyzer', 'zls', 'marksman', 'rnix' };
 
 return {
   lsp_servers = lsp_servers
