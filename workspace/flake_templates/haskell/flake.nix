7709931821c4bb8572614b4064784cc2502fa07a
v4zha <arvin_j4848@protonmail.com>
2024-11-07 21:05:59 +0530

    stow-ignore-local

---
+++ workspace/flake_templates/haskell/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "v4zha's Haskell flakes";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    my-package = "";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem = { self', pkgs, ... }: {
+
+        haskellProjects.default = {
+          packages = { };
+          devShell = {
+            enable = true;
+            tools = hp: {
+              fourmolu = hp.fourmolu;
+            };
+            hlsCheck.enable = true;
+          };
+        };
+
+        packages.default = self'.packages.${my-package};
+      };
+    };
+}
