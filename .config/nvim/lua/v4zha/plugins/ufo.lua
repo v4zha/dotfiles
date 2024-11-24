af0734051fc224a8743f5b28a7818cec11a3712e
v4zha <arvin_j4848@protonmail.com>
2024-11-24 11:41:34 +0530

    nvim:ufold-signcolumn number rem

--- .config/nvim/lua/v4zha/plugins/ufo.lua
+++ .config/nvim/lua/v4zha/plugins/ufo.lua
@@ -1,7 +1,9 @@
-vim.o.foldcolumn = '1'
+vim.o.foldcolumn = 'auto:9'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
 
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
