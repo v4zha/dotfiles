d9c98b36c285d8230e53c8d22e8e9e3219ba73ca
v4zha <arvin_j4848@protonmail.com>
2025-01-26 11:42:07 +0530

    chore

--- .config/hypr/scripts/sleep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# swayidle -w \
-#     timeout 120 'hyprlock' \
-#     timeout 480 'hyprctl dispatch dpms off ' \
-#     timeout 1200 'systemctl suspend' \
-#     resume 'hyprctl dispatch dpms on' \
-#     before-sleep 'hyprlock'
--- .config/nvim/lua/v4zha/plugins/lualine.lua
+++ .config/nvim/lua/v4zha/plugins/lualine.lua
@@ -1,42 +1,42 @@
 local status, lualine = pcall(require, "lualine")
 if not status then
-    return
+  return
 end
 
 local function vscode()
-    return 'V4zha Studio Code Pro'
+  return 'V4zha Studio Code Pro'
 end
 
 
 lualine.setup {
-    options = {
-        theme = 'kanagawa',
-        disabled_filetypes = { 'packer' },
-        extensions = { 'man', 'neo-tree','lazy','nvim-dap-ui' ,'fzf'},
-        section_separators = { left = '', right = '' },
-        -- component_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        globalstatus = true,
-        always_divide_middle = true
-    },
-    tabline = {
-        lualine_a = {
-            {
-                'buffers',
-                buffers_color = {
-                    active = 'lualine_a_normal',
-                    inactive = 'lualine_z_inactive',
-                    separator = "|"
-                },
-            }
+  options = {
+    theme = 'kanagawa',
+    disabled_filetypes = { 'packer' },
+    extensions = { 'man', 'neo-tree', 'lazy', 'nvim-dap-ui', 'fzf' },
+    section_separators = { left = '', right = '' },
+    -- component_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    globalstatus = true,
+    always_divide_middle = true
+  },
+  tabline = {
+    lualine_a = {
+      {
+        'buffers',
+        buffers_color = {
+          active = 'lualine_a_normal',
+          inactive = 'lualine_z_inactive',
+          separator = "|"
         },
-        lualine_z = {
-            { vscode,
-                icons_enabled = true,
-                icon = { '󰘧', align = 'right' },
-            }
-        }
+      }
     },
+    lualine_z = {
+      { vscode,
+        icons_enabled = true,
+        icon = { '󰘧', align = 'right' },
+      }
+    }
+  },
   sections = {
     lualine_x = {
       {
@@ -47,4 +47,3 @@ lualine.setup {
     },
   },
 }
-
